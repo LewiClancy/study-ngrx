1f13b1a6de724c4a77318d5f3d537f983af7faaa
LewiClancy <itslewisndungu@gmail.com>
2021-09-07 23:10:34 +0300

    1 file modified

--- src/app/store/counter/counter.effects.ts
+++ src/app/store/counter/counter.effects.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Actions, ofType, createEffect } from '@ngrx/effects';
 import { EMPTY } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { SnackbarService } from '../../shared/snackbar.service';
 
 @Injectable()
 export class CounterEffects {
   constructor(private actions$: Actions, private snackbar: SnackbarService) {}
 
-  showSnackbar$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType('[Counter] Reset'),
-      map(() => {
-        this.snackbar.openSnackBar('Your counter has been reset.');
-        return EMPTY;
-      })
-    )
-  , {dispatch: false});
+  showSnackbar$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType('[Counter] Reset'),
+        map(() => {
+          this.snackbar.openSnackBar('Your counter has been reset.');
+          return EMPTY;
+        })
+      ),
+    { dispatch: false }
+  );
 }
